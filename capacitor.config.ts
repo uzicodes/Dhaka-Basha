17fef826649deed725e82dc87232d60ea9dbe04a
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2026-06-20 22:21:03 +0600

    chore: installed 'capacitor' package

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dhakabasha.app',
+  appName: 'Dhaka Basha',
+  webDir: 'public'
+};
+
+export default config;
